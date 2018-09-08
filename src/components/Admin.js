4a1597262abfefcd16154081fff457496dbd68b3
waelvilla <waelvilla@gmail.com>
2018-09-08 23:51:18 +0300

    handleSubmit Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -5,16 +5,30 @@ class Admin extends React.Component{
 
     
     handleSubmit(role){
-        let Employees= ''
+        let Persons= ''
+        let roles=this.props.users
+        for(var r in roles){
+            if(r==role){
+                Persons=roles[r]
+                break
+            } 
+        }
+        console.log(Persons);
+        
+        
         
     }
     render(){
         if(this.props.active)
             return(
                 <div>
-                    <button onClick={()=>this.handleSubmit('Employees')}>Manage Employees</button>
+                    <button onClick={()=>this.handleSubmit('Doctor')}>Manage Doctors</button>
                     <br />
-                    <button onClick={()=>this.handleSubmit('Patients')}>Manage Patients</button>
+                    <button onClick={()=>this.handleSubmit('Nurse')}>Manage Nurses</button>
+                    <br />
+                    <button onClick={()=>this.handleSubmit('Admin')}>Manage Admins</button>
+                    <br />
+                    <button onClick={()=>this.handleSubmit('Patient')}>Manage Patients</button>
                     
                 </div>
             )
